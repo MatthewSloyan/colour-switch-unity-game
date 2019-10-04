7ecb453f3bcb9554623d98a4caa615d71d6a2e25
Matthew Sloyan <g00348036@gmit.ie>
2019-10-04 16:19:58 +0100

    Set up tags and colliders for the spinner.

---
+++ G00348036/Assets/__Scripts/PlayerCollider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // When the ball collides with a piece of the spinner this method is triggered.
+    private void OnTriggerEnter2D (Collider2D collision)
+    {
+        // Output tag of piece it collided with.
+        Debug.Log(collision.tag);
+    }
+}

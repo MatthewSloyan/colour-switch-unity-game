c45caf87332fdbd70ebd86dcc986c7d83b3ed614
Matthew Sloyan <g00348036@gmit.ie>
2019-10-04 15:38:50 +0100

    Set up of spinner sprite and basic rotation.

--- G00348036/Assets/__Scripts/Player.cs
+++ G00348036/Assets/__Scripts/Player.cs
@@ -23,8 +23,9 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        // Get input from the keyboard or left mouse click to make the ball jump up
-        if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0)){
+        // Get space bar, up arrow and left mouse click input
+        if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
             // Set the velocity of the Rigid body to the current velocity times the jump speed.
             rb.velocity = Vector2.up * jumpSpeed;
         }
---
+++ G00348036/Assets/__Scripts/SpinnerRotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnerRotator : MonoBehaviour {
+   
+    #region == Private Variables == 
+    [SerializeField]
+    private float rotationSpeed = 100f;
+
+    #endregion
+
+    // Update is called once per frame
+    void Update () {
+        transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
+	}
+}

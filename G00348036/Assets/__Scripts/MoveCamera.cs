6d06092aa4ed0eb1ea769c0fc64962570ba6ad62
Matthew Sloyan <g00348036@gmit.ie>
2019-10-06 11:26:19 +0100

    Added camera movement to follow the player when it passes the half way point.

---
+++ G00348036/Assets/__Scripts/MoveCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+    #region == Private Variables == 
+    [SerializeField]
+    private Transform playerObject; // Get the player
+
+    #endregion
+
+    // Update is called once per frame
+    void Update () {
+        // If the player goes above a half way the camera will move with the player
+        if (playerObject.position.y > transform.position.y)
+        {
+            // Set the camera to the new player position.
+            transform.position = new Vector3(transform.position.x, playerObject.position.y, transform.position.z);
+        }
+	}
+}

a4fd0ac3fad7fda2e69495e0c7867a91b28b668e
Matthew Sloyan <g00348036@gmit.ie>
2019-10-06 12:35:09 +0100

    Started to implement the ability to change player colour.

--- G00348036/Assets/__Scripts/ColourManager.cs
+++ G00348036/Assets/__Scripts/ColourManager.cs
@@ -1,25 +1,47 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ColourManager : MonoBehaviour {
-    
+
     #region == Private Variables == 
     [SerializeField]
     private Color[] colours; // Array of colours
+
+   // Color[] colors = { new Color(0, 1, 0, 1), new Color(1, 0, 0, 1), new Color(1, 1, 1, 1), new Color(0, 0, 1, 1), new Color(1, 1, 0, 1), new Color(0, 0, 0, 1) };
 
     [SerializeField]
     private SpriteRenderer sr;
 
     private string[] colourOptions = new string[4] { "GreenTag", "YellowTag", "BlueTag", "RedTag" }; // Array of colour tags to set
 
+
+    //create an object of SingleObject
+    private static ColourManager instance = new ColourManager();
+
+    //make the constructor private so that this class cannot be
+    //instantiated
+    private ColourManager() {
+    }
+
+    //Get the only object available
+    public static ColourManager getInstance()
+    {
+        return instance;
+    }
+
     #endregion
     
     // Use this for initialization
     void Start () {
+        setRandomColour();
+    }
 
+    public void setRandomColour()
+    {
         // Get a random index between 1 and 4
-        int randomColour = Random.Range(0, colours.Length);
+        int randomColour = UnityEngine.Random.Range(0, colours.Length);
 
         // Set the colour to one of the determined colours in Unity
         sr.color = colours[randomColour];
@@ -29,8 +51,12 @@ public class ColourManager : MonoBehaviour {
         sr.tag = colourOptions[randomColour];
     }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+    public void setColour()
+    {
+        int index = Array.IndexOf(colourOptions, sr.tag);
+        
+        sr.color = colours[index];
+
+        sr.tag = colourOptions[index];
+    }
 }
--- G00348036/Assets/__Scripts/PlayerCollider.cs
+++ G00348036/Assets/__Scripts/PlayerCollider.cs
@@ -11,6 +11,18 @@ public class PlayerCollider : MonoBehaviour {
     // When the ball collides with a piece of the spinner this method is triggered.
     private void OnTriggerEnter2D (Collider2D collision)
     {
+        ColourManager c = ColourManager.getInstance();
+
+        //Debug.Log(collision.tag);
+
+        if (collision.name == "ColourChanger")
+        {
+            //c.setColour();
+            Debug.Log("Change Colour!");
+            Destroy(collision.gameObject);
+            return;
+        }
+
         // If the current player tag (spriteRender tag) is equal to the spinner piece tag then end the game.
         // Otherwise allow the player through
         if (collision.tag != sr.tag)

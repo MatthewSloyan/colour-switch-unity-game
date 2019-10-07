3c794e27a5a5237f0901d694f0cd9ba3d1f2895c
Matthew Sloyan <g00348036@gmit.ie>
2019-10-07 13:27:38 +0100

    Added basic pause menu UI and the ability to pause/resume game.

---
+++ G00348036/Assets/__Scripts/PauseMenu.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    
+
+    #region == Private Variables == 
+    [SerializeField]
+    private static bool isGamePaused = false;
+
+    [SerializeField]
+    private GameObject pauseMenuUI;
+
+    #endregion
+
+    // Update is called once per frame
+    void Update () {
+
+        // Get esc key input from keyboard
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    // Resumes game if called
+    public void ResumeGame()
+    {
+        // Turn off the menu UI
+        pauseMenuUI.SetActive(false);
+
+        // Start the game running again
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    // Pauses game if called
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void GoToHomeMenu()
+    {
+
+    }
+}

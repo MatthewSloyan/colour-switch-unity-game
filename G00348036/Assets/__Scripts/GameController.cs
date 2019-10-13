90dc2f7d629ffd5e4295bf60514d289323ca15f2
Matthew Sloyan <g00348036@gmit.ie>
2019-10-13 17:59:48 +0100

    Started to implement the ability to instantiate game objects at run time.

--- G00348036/Assets/__Scripts/GameController.cs
+++ G00348036/Assets/__Scripts/GameController.cs
@@ -11,22 +11,39 @@ public class GameController : MonoBehaviour
     [SerializeField]
     private GameObject colourSwapper;
 
+    [SerializeField]
+    private GameObject scoreStar;
+
+    [SerializeField]
+    private Transform prevSpinner;
+
     // Use this for initialization
     void Start()
     {
-        //createColourSwapper();
-        //createSpinner();
+        createGameObjects();
         ColourManager.Instance.setPlayerColour();
     }
 
-    private void createSpinner()
+    private void createGameObjects()
     {
+        Vector2 newSpinnerPos = prevSpinner.position;
+        newSpinnerPos.y += 5f;
 
+        Vector2 newStarPos = prevSpinner.position;
+        newStarPos.y += 5f;
+
+        Vector2 newcolourSwapperPos = newSpinnerPos;
+        newcolourSwapperPos.y += 2.5f;
+
+        GameObject newSpinner = Instantiate(spinner, newSpinnerPos, Quaternion.identity);
+        Instantiate(scoreStar, newStarPos, Quaternion.identity);
+        Instantiate(colourSwapper, newcolourSwapperPos, Quaternion.identity);
+        prevSpinner = newSpinner.transform;
     }
 
-    private void createColourSwapper()
-    {
-        colourSwapper.tag = "MainCamera";
-        Instantiate(colourSwapper, new Vector2(transform.position.x, transform.position.y + 5), Quaternion.identity);
-    }
+    //private void createColourSwapper()
+    //{
+    //    colourSwapper.tag = "MainCamera";
+    //    Instantiate(colourSwapper, new Vector2(transform.position.x, transform.position.y + 5), Quaternion.identity);
+    //}
 }

f25f1d9ee90ad5358eb9fcd59b44a1242bf17831
Matthew Sloyan <g00348036@gmit.ie>
2019-11-17 13:45:10 +0000

    Started to implement slider obstacle.

---
+++ G00348036/Assets/__Scripts/SliderMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderMovement : MonoBehaviour {
+
+    #region == Private Variables == 
+    [SerializeField]
+    private float movementSpeed;
+
+    [SerializeField]
+    private GameObject slider;
+
+    private float outOfBounds = 8.45f;
+    #endregion
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 temp = new Vector3(movementSpeed, 0, 0);
+        transform.position += temp;
+
+        if (transform.position.x > outOfBounds)
+        {
+            transform.position = new Vector3(2.8f, transform.position.y, 0);
+            Debug.Log("Test");
+            //Vector2 newSliderPos = slider.transform.position;
+            //newSliderPos.x = -5.6f;
+
+            //Instantiate(slider, newSliderPos, Quaternion.identity);
+
+            //Destroy(slider);
+        }
+    }
+}

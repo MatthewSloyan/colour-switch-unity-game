8320d40eab1493d33e405c073f1f46c34b5b1a19
Matthew Sloyan <g00348036@gmit.ie>
2019-11-28 17:20:40 +0000

    Added more to level implementation. Spinners and sliders can now be created dynamically.

--- G00348036/Assets/__Scripts/GameController.cs
+++ G00348036/Assets/__Scripts/GameController.cs
@@ -60,20 +60,20 @@ public class GameController : MonoBehaviour
         // == STAR SCORE == 
         Instantiate(scoreStar, spinnerPos, Quaternion.identity);
 
-        // Finally set the old position of the spinner to the new position for following calls.
-        // So that they spawn correctly in order.
-        prevPosition = newSpinner.transform;
+        //// Finally set the old position of the spinner to the new position for following calls.
+        //// So that they spawn correctly in order.
+        //prevPosition = newSpinner.transform;
 
-        // == SPINNER == 
-        // Create a position for the new Spinner using the previous location
-        spinnerPos = prevPosition.position;
-        spinnerPos.y += 5.2f;
+        //// == SPINNER == 
+        //// Create a position for the new Spinner using the previous location
+        //spinnerPos = prevPosition.position;
+        //spinnerPos.y += 5f;
 
-        // Instantiate spinner and set the previous position to the new position for the next call.
-        newSpinner = Instantiate(spinner, spinnerPos, Quaternion.identity);
+        //// Instantiate spinner and set the previous position to the new position for the next call.
+        //newSpinner = Instantiate(spinner, spinnerPos, Quaternion.identity);
 
-        // == STAR SCORE == 
-        Instantiate(scoreStar, spinnerPos, Quaternion.identity);
+        //// == STAR SCORE == 
+        //Instantiate(scoreStar, spinnerPos, Quaternion.identity);
 
         // == COLOUR SWAPPER == 
         // Create a position for the new Colour Swapper using the new spinner location
@@ -84,49 +84,77 @@ public class GameController : MonoBehaviour
         // Finally set the old position of the spinner to the new position for following calls.
         // So that they spawn correctly in order.
         prevPosition = newSpinner.transform;
+
+        PlayerPrefs.SetInt("LevelSwitch", 1);
+
+        //PlayerPrefs.DeleteKey("LevelSwitch");
     }
 
     // Create all three core game ojects (Spinners, Colour Swappers and Star Scores
-    public void createGameObjects(int levelSwitch)
+    public void createGameObjects()
     {
+        int levelSwitch = PlayerPrefs.GetInt("LevelSwitch");
+
         if (levelSwitch == 0)
         {
+            Vector2 spinnerPos;
+            GameObject newSpinner;
+
             // == SPINNER == 
             // Create a position for the new Spinner using the previous location
-            Vector2 newSpinnerPos = prevPosition.position;
-            newSpinnerPos.y += 5f;
+            spinnerPos = prevPosition.position;
+            spinnerPos.y += 5f;
 
             // Instantiate spinner and set the previous position to the new position for the next call.
-            GameObject newSpinner = Instantiate(spinner, newSpinnerPos, Quaternion.identity);
+            newSpinner = Instantiate(spinner, spinnerPos, Quaternion.identity);
 
             // == STAR SCORE == 
-            // Create a position for the new Star Score object using the previous spinner location (center)
-            Vector2 newStarPos = prevPosition.position;
-            newStarPos.y += 5f;
+            Instantiate(scoreStar, spinnerPos, Quaternion.identity);
 
-            Instantiate(scoreStar, newStarPos, Quaternion.identity);
+            //// Finally set the old position of the spinner to the new position for following calls.
+            //// So that they spawn correctly in order.
+            //prevPosition = newSpinner.transform;
+
+            //// == SPINNER == 
+            //// Create a position for the new Spinner using the previous location
+            //spinnerPos = prevPosition.position;
+            //spinnerPos.y += 5f;
+
+            //// Instantiate spinner and set the previous position to the new position for the next call.
+            //newSpinner = Instantiate(spinner, spinnerPos, Quaternion.identity);
+
+            //// == STAR SCORE == 
+            //Instantiate(scoreStar, spinnerPos, Quaternion.identity);
 
             // == COLOUR SWAPPER == 
             // Create a position for the new Colour Swapper using the new spinner location
-            Vector2 newcolourSwapperPos = newSpinnerPos;
-            newcolourSwapperPos.y += 2.5f;
+            spinnerPos.y += 2.5f;
 
-            Instantiate(colourSwapper, newcolourSwapperPos, Quaternion.identity);
+            Instantiate(colourSwapper, spinnerPos, Quaternion.identity);
 
             // Finally set the old position of the spinner to the new position for following calls.
             // So that they spawn correctly in order.
             prevPosition = newSpinner.transform;
+
+            PlayerPrefs.SetInt("LevelSwitch", 1);
         }
         else
         {
-            // == SLIDER == 
-            // Create a position for the new Spinner using the previous location
-            Vector2 newSliderPos = prevPosition.position;
-            newSliderPos.x = 2.81f;
-            newSliderPos.y += 3f;
+            for (int i = 0; i < 2; i++)
+            {
+                // == SLIDER == 
+                // Create a position for the new Spinner using the previous location
+                Vector2 newSliderPos = prevPosition.position;
+                newSliderPos.x = 2.81f;
+                newSliderPos.y += 3f;
 
-            // Instantiate spinner and set the previous position to the new position for the next call.
-            GameObject newSlider = Instantiate(slider, newSliderPos, Quaternion.identity);
+                // Instantiate spinner and set the previous position to the new position for the next call.
+                GameObject newSlider = Instantiate(slider, newSliderPos, Quaternion.identity);
+
+                prevPosition = newSlider.transform;
+            }
+
+            PlayerPrefs.SetInt("LevelSwitch", 0);
         }
     }
 }

689e01fbdc7d9c0bd7746039b7cd067508708ca1
Matthew Sloyan <g00348036@gmit.ie>
2019-11-29 16:00:36 +0000

    Refactored game controller code to use one method rather than two, and added comments.

--- G00348036/Assets/__Scripts/GameController.cs
+++ G00348036/Assets/__Scripts/GameController.cs
@@ -17,9 +17,11 @@ public class GameController : MonoBehaviour
 
     [SerializeField]
     private GameObject scoreStar;
-    
+
+    // Previous gameObject postion
     private Transform prevPosition;
 
+    // parent game object container
     private GameObject parent;
 
     #endregion
@@ -38,103 +40,59 @@ public class GameController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        // Set up parent game object container
         parent = GameObject.Find("GameObjectContainer");
-
-        //createGameObjects(0);
-        createInitial();
+        
+        // Set level switch to 0 to load spinners, and create inital game objects
+        PlayerPrefs.SetInt("LevelSwitch", 0);
+        createGameObjects();
 
         // Set initial player colour
         ColourManager.Instance.setPlayerColour();
     }
 
-    // Create all three core game ojects (Spinners, Colour Swappers and Star Scores
-    public void createInitial()
-    {
-        Vector2 spinnerPos;
-        GameObject newSpinner;
-
-        // == SPINNER == 
-        // Create a position for the new Spinner using the previous location
-        spinnerPos = new Vector2(0, 1.2f);
-
-        // Instantiate spinner and set as child to parent
-        // Code adapted from: https://docs.unity3d.com/ScriptReference/Transform.SetParent.html
-        newSpinner = Instantiate(spinner, spinnerPos, Quaternion.identity);
-        //newSpinner.transform.parent = parent.transform;
-        newSpinner.transform.SetParent(parent.transform, false);
-
-        // == STAR SCORE == 
-        Instantiate(scoreStar, spinnerPos, Quaternion.identity).transform.SetParent(parent.transform, false);
-
-        //// Finally set the old position of the spinner to the new position for following calls.
-        //// So that they spawn correctly in order.
-        //prevPosition = newSpinner.transform;
-
-        //// == SPINNER == 
-        //// Create a position for the new Spinner using the previous location
-        //spinnerPos = prevPosition.position;
-        //spinnerPos.y += 5f;
-
-        //// Instantiate spinner and set the previous position to the new position for the next call.
-        //newSpinner = Instantiate(spinner, spinnerPos, Quaternion.identity);
-
-        //// == STAR SCORE == 
-        //Instantiate(scoreStar, spinnerPos, Quaternion.identity);
-
-        // == COLOUR SWAPPER == 
-        // Create a position for the new Colour Swapper using the new spinner location
-        spinnerPos.y += 2.5f;
-
-        Instantiate(colourSwapper, spinnerPos, Quaternion.identity).transform.SetParent(parent.transform, false);
-
-        // Finally set the old position of the spinner to the new position for following calls.
-        // So that they spawn correctly in order.
-        prevPosition = newSpinner.transform;
-
-        PlayerPrefs.SetInt("LevelSwitch", 1);
-    }
-
-    // Create all three core game ojects (Spinners, Colour Swappers and Star Scores
+    // Create all core game ojects (Spinners, sliders, Colour Swappers and Star Scores)
     public void createGameObjects()
     {
+        // Get the type of level to load, 0 = spinners, 1 = sliders
         int levelSwitch = PlayerPrefs.GetInt("LevelSwitch");
-        Debug.Log("Level Num: " + levelSwitch);
 
         if (levelSwitch == 0)
         {
-            Vector2 spinnerPos;
-            GameObject newSpinner;
+            Vector2 spinnerPos = new Vector2(0, 0);
+            GameObject newSpinner = null;
 
             // == SPINNER == 
-            // Create a position for the new Spinner using the previous location
-            spinnerPos = prevPosition.position;
-            spinnerPos.y += 3f;
+            for (int i = 0; i < 2; i++)
+            {
+                // If very first spinner then set manually.
+                if (prevPosition == null)
+                {
+                    spinnerPos = new Vector2(0, 1.2f);
+                }
+                else
+                {
+                    // Set the position for new Spinner using the previous location.
+                    // Add three to positon it correctly above last gameObject.
+                    spinnerPos = prevPosition.position;
+                    spinnerPos.y += 5f;
+                }
 
-            // Instantiate spinner and set the previous position to the new position for the next call.
-            newSpinner = Instantiate(spinner, spinnerPos, Quaternion.identity);
-            newSpinner.transform.SetParent(parent.transform, false);
+                // Instantiate new spinner using position and add to parent container.
+                newSpinner = Instantiate(spinner, spinnerPos, Quaternion.identity);
+                newSpinner.transform.SetParent(parent.transform, false);
 
-            // == STAR SCORE == 
-            Instantiate(scoreStar, spinnerPos, Quaternion.identity).transform.SetParent(parent.transform, false);
+                // == STAR SCORE == 
+                // Instantiate new star using same position and add to parent container. As stars are in the center of spinners
+                Instantiate(scoreStar, spinnerPos, Quaternion.identity).transform.SetParent(parent.transform, false);
 
-            //// Finally set the old position of the spinner to the new position for following calls.
-            //// So that they spawn correctly in order.
-            //prevPosition = newSpinner.transform;
-
-            //// == SPINNER == 
-            //// Create a position for the new Spinner using the previous location
-            //spinnerPos = prevPosition.position;
-            //spinnerPos.y += 5f;
-
-            //// Instantiate spinner and set the previous position to the new position for the next call.
-            //newSpinner = Instantiate(spinner, spinnerPos, Quaternion.identity);
-
-            //// == STAR SCORE == 
-            //Instantiate(scoreStar, spinnerPos, Quaternion.identity);
-
-            // == COLOUR SWAPPER == 
-            // Create a position for the new Colour Swapper using the new spinner location
-            spinnerPos.y += 2.5f;
+                // Set prevPosition to the current slider, for next slider or next level.
+                prevPosition = newSpinner.transform;
+            }
+            
+            // == COLOUR SWAPPER ==
+            // Create a position for the new Colour Swapper using the same postion plus 2.75
+            spinnerPos.y += 2.75f;
 
             Instantiate(colourSwapper, spinnerPos, Quaternion.identity).transform.SetParent(parent.transform, false);
 
@@ -147,41 +105,52 @@ public class GameController : MonoBehaviour
         else
         {
             Vector2 sliderPos = new Vector2(0, 0);
+            GameObject newSlider = null;
 
-            for (int i = 0; i < 2; i++)
+            // == SLIDER == 
+            // Create three sliders equaly spaced from each other
+            for (int i = 0; i < 3; i++)
             {
-                // == SLIDER == 
-                // Create a position for the new Spinner using the previous location
+                // Get the previous gameObject postion and set x so it starts off screen.
                 sliderPos = prevPosition.position;
                 sliderPos.x = 2.81f;
-                sliderPos.y += 4f;
 
-                // Instantiate spinner and set the previous position to the new position for the next call.
-                GameObject newSlider = Instantiate(slider, sliderPos, Quaternion.identity);
+                // If first slider, set sligthly higher so it's not too close to the colour swapper.
+                if (i == 0)
+                    sliderPos.y += 4.5f;
+                else
+                    sliderPos.y += 3f;
+
+                // Instantiate new slider using prev position and add to parent container.
+                newSlider = Instantiate(slider, sliderPos, Quaternion.identity);
                 newSlider.transform.SetParent(parent.transform, false);
 
-                if (i < 1)
+                // == STAR SCORE == 
+                // Add a star score to the parent in the center only for the first two sliders.
+                if (i < 2)
                 {
-                    // == STAR SCORE == 
+                    // Set back to 0 so star is centered.
                     sliderPos.x = 0;
-                    sliderPos.y += 2;
+                    sliderPos.y += 1.5f;
                     Instantiate(scoreStar, sliderPos, Quaternion.identity).transform.SetParent(parent.transform, false);
-                    //newColourSwap.transform.SetParent(parent.transform, false);
                 }
 
+                // Set prevPosition to the current slider, for next slider or next level.
                 prevPosition = newSlider.transform;
             }
 
             // == COLOUR SWAPPER == 
-            // Create a position for the new Colour Swapper using the new spinner location
+            // Set back to 0 so star is centered, and move up.
             sliderPos.x = 0;
             sliderPos.y += 2f;
 
-            GameObject newColourSwap = Instantiate(colourSwapper, sliderPos, Quaternion.identity);
-            newColourSwap.transform.SetParent(parent.transform, false);
+            Instantiate(colourSwapper, sliderPos, Quaternion.identity).transform.SetParent(parent.transform, false);
 
-            prevPosition = newColourSwap.transform;
+            // Finally set the old position to the new position for following calls.
+            // So that they spawn correctly in order.
+            prevPosition = newSlider.transform;
 
+            // Set level switch so it will load spinners on next call.
             PlayerPrefs.SetInt("LevelSwitch", 0);
         }
     }
